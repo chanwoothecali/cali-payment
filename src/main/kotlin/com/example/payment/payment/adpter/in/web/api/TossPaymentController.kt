6255bc5b9a095f8ae52e517cd0157a4e28baa30a
chanwoothecali <jungcali94@gmail.com>
2024-07-01 18:09:44 +0900

    토스 결제 성공 1차 작업

---
+++ src/main/kotlin/com/example/payment/payment/adpter/in/web/api/TossPaymentController.kt
@@ -0,0 +1,32 @@
+package com.example.payment.payment.adpter.`in`.web.api
+
+import com.example.payment.common.WebAdapter
+import com.example.payment.payment.adpter.out.web.executor.TossPaymentExecutor
+import com.example.payment.payment.adpter.out.web.response.ApiResponse
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
+
+@WebAdapter
+@RequestMapping("/v1/toss")
+@RestController
+class TossPaymentController (
+    private val tossPaymentExecutor: TossPaymentExecutor
+) {
+
+    @PostMapping("/confirm")
+    fun confirm(request: TossPaymentRequestConfirm): Mono<ResponseEntity<ApiResponse<String>>> {
+        return tossPaymentExecutor.execute(
+            paymentKey = request.paymentKey,
+            orderId = request.orderId,
+            amount = request.amount.toString()
+        ).map {
+            ResponseEntity.ok().body(
+                ApiResponse.with(HttpStatus.OK, "ok", it)
+            )
+        }
+    }
+}
---
+++ src/main/kotlin/com/example/payment/payment/adpter/out/web/response/ApiResponse.kt
@@ -0,0 +1,17 @@
+package com.example.payment.payment.adpter.out.web.response
+
+import org.springframework.http.HttpStatus
+
+data class ApiResponse<T> (
+    val status: Int = 200,
+    val message: String = "",
+    val data: T? = null
+) {
+
+    companion object {
+
+        fun <T> with(httpStatus: HttpStatus, message: String, data: T?): ApiResponse<T> {
+            return ApiResponse(status = httpStatus.value(), message = message, data = data)
+        }
+    }
+}
